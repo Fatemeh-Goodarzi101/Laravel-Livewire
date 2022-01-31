f8fbca5d8f04f9a1b320ad3fed28a99eac5a155e
Fatemeh Goodarzi <fatemehgoodarzi101@gmail.com>
2022-01-31 15:27:03 +0330

    base commit

---
+++ app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Jetstream\Jetstream;
+
+class CreateNewUser implements CreatesNewUsers
+{
+    use PasswordValidationRules;
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array  $input
+     * @return \App\Models\User
+     */
+    public function create(array $input)
+    {
+        Validator::make($input, [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'national_code' => ['required', 'numeric'],
+            'phone_number' => ['required', 'numeric'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => $this->passwordRules(),
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
+        ])->validate();
+
+        return User::create([
+            'first_name' => $input['first_name'],
+            'last_name' => $input['last_name'],
+            'national_code' => $input['national_code'],
+            'phone_number' => $input['phone_number'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
+        ]);
+    }
+}
---
+++ app/Http/Livewire/Admin/User/Create.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Livewire\Admin\User;
+
+use App\Models\user;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class Create extends Component
+{
+    use WithFileUploads;
+
+    
+    protected $rules = [
+        
+    ];
+
+    public function updated($input)
+    {
+        $this->validateOnly($input);
+    }
+
+    public function create()
+    {
+        $this->validate();
+
+        $this->dispatchBrowserEvent('show-message', ['type' => 'success', 'message' => __('CreatedMessage', ['name' => __('User') ])]);
+        
+        User::create([
+            'user_id' => auth()->id(),
+        ]);
+
+        $this->reset();
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.user.create')
+            ->layout('admin::layouts.app', ['title' => __('CreateTitle', ['name' => __('User') ])]);
+    }
+}
